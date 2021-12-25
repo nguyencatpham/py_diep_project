d6656fdc4648c7ca7929ce9551649c3d38eb4429
toanitsv <khoailangdaknong@hotmail.com>
2021-12-25 16:08:34 +0700

    revise setting

--- diep_project/settings.py
+++ diep_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'diep_website.apps.DiepWebsiteConfig',
     'diep_website',
 ]
 
