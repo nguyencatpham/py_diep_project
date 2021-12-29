54d10cf8076546e2cd55b127c7c8766c5d3760e5
Toan Nguyen <nguyentoan712@gmail.com>
2021-12-30 00:30:31 +0700

    complete navbar

--- diep_website/urls.py
+++ diep_website/urls.py
@@ -5,9 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
-    path('index', views.index, name='index'),
-    path('navbar', views.navbar, name='navbar'),
-    path('base', views.base, name='base'),
     path('article/<str:pk>', views.article_detail, name='article'),
     path('backend/checkproductcode', views.checkProductCode, name='checkProductCode'),
     path('checkqrcode/<str:pk>', views.checkqrcode, name='article_checkqrcode'),
--- diep_website/views.py
+++ diep_website/views.py
@@ -4,14 +4,9 @@ from .models import Product, Article
 from django.http import HttpResponse
 from .resources import ProductResource
 
-def navbar(request):
-    return render(request, 'navbar2.html')
-
-def base(request):
-    return render(request, 'base-2.html')
 # Create your views here.
 def index(request):
-    return render(request, 'home-1.html')
+    return render(request, 'home.html')
 
 # Create your views here.
 def home(request):
@@ -21,34 +16,28 @@ def home(request):
 def article_detail(request, pk):
     articleObj = Article.objects.get(id=pk)
     articles = Article.objects.all()
-    return render(request, 'article-detail-new-2.html',{"article": articleObj, "articles": articles})
+    return render(request, 'article-detail.html',{"article": articleObj, "articles": articles})
  
 def checkqrcode(request, pk):
     productObj = Product.objects.get(product_code = pk)
-    articleObj = Article.objects.get(id = productObj.article_id)
     if productObj:
-        return render(request, 'article-detail-checkqrcode-new.html', {"product": productObj, "article": articleObj})
+        articleObj = Article.objects.get(id = productObj.article_id)
+        return render(request, 'article-detail-checkqrcode.html', {"product": productObj, "article": articleObj})
     else:
         return render(request, '404.html')
 
 def checkProductCode(request):
-    # request should be ajax and method should be GET.request.is_ajax and 
     if request.method == "GET":
-        # get the nick name from the client side.
         check_what = request.GET.get("check_what", None)
         if check_what != None:
-            # check for the nick name in the database.
-            productObj = Product.objects.get(product_code = check_what)
-            if productObj:
-                # if nick_name found return not valid new friend
+            if Product.objects.filter(product_code = check_what).exists():
+                productObj = Product.objects.get(product_code = check_what)
                 return JsonResponse({"valid":True, "serial_no":productObj.serial_no}, status = 200)
             else:
-                # if nick_name not found, then user can create a new friend.
                 return JsonResponse({"valid":False}, status = 200)
         else:
             return render(request, '404.html')
     return render(request, '404.html')
-    # return JsonResponse({}, status = 400)
 
 def export(request):
     product_resource = ProductResource()
